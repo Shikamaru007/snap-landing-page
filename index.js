cb3341d31d74b2047679ceca6b360b2256927565
froZin <bolutolu12@gmail.com>
2024-11-07 20:32:09 +0100

    mobile view done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const openMenu = document.getElementById("menu");
+const closeMenu = document.getElementById("closeMenu");
+const dropDowns = document.querySelectorAll(".dropdown");
 
+openMenu.addEventListener("click", () => {
+    document.body.classList.add("open-nav");
+});
+closeMenu.addEventListener("click", () => {
+    document.body.classList.remove("open-nav");
+});
+dropDowns.forEach(dropdown => {
+    dropdown.addEventListener("click", (event) => {
+        event.target.classList.toggle("preview");
+       let option = dropdown.parentNode.querySelector(".options");
+       if(dropdown.classList.contains("preview")){
+        option.style.height = `${option.scrollHeight}px`;
+       }else{
+        option.style.height = "0px";
+       }
+      
+    });
+})
